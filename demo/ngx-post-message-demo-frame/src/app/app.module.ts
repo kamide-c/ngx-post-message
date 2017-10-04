49a9377de41424a932caa92da5b5d4e5c8b63b4b
apoterenko <a.poterenko@gmail.com>
2017-10-04 16:48:47 +0300

    *demo {part 2}

--- demo/ngx-post-message-demo-frame/src/app/app.module.ts
+++ demo/ngx-post-message-demo-frame/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { PostMessageModule } from 'ngx-post-message';
 
 import { AppComponent } from './app.component';
 
@@ -9,8 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    PostMessageModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- demo/ngx-post-message-demo/src/app/app.component.ts
+++ demo/ngx-post-message-demo/src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+
+import { IPostMessageBridge, IPostMessageEventTarget, PostMessageBridgeFactory } from 'ngx-post-message/index';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(@Inject(PostMessageBridgeFactory) private bridgeFactory: PostMessageBridgeFactory) {
+  }
+
+  public ngOnInit() {
+
+    /**
+     * Root context
+     */
+    const iFrame: IPostMessageEventTarget = window.frames[0];
+    const currentWindow: IPostMessageEventTarget = window;
+
+    // The main usage scenario
+    const bridge: IPostMessageBridge = this.bridgeFactory.makeInstance()
+      .connect(currentWindow, iFrame)
+      .makeBridge('Logout')
+      .makeBridge('ChangeLanguage')
+      .addListener('Logout', (message: any) =>
+        console.debug(`[Parent][Logout listener] The child has sent an empty message <Logout> to the parent`));
+
+    setTimeout(() => {
+      bridge.sendMessage('ChangeLanguage', 'ru');
+
+      setTimeout(() => {
+        // The additional usage scenario
+        // You can also use the direct native mechanism of sending the message (if the external application does not use Angular2)
+        window.frames[0].postMessage([{channel: 'ChangeLanguage', message: 'de'}], '*');
+      }, 1000);
+    }, 2000);
+  }
 }
